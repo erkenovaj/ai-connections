7690626f77b5e577d0aaf762c964f0c0b7e2d652
Джамиля Эркенова <DM.Erkenova@ispras.ru>
2026-02-14 16:09:46 +0300

    added db

--- js/room-config.js
+++ js/room-config.js
@@ -10,7 +10,7 @@
  */
 const ROOM_CONFIG = {
     SUPABASE_URL: 'https://ypbgvpqjnuorwqwjloif.supabase.co',
-    SUPABASE_ANON_KEY: 'sb_publishable__hWZt2XYzP7-0ZtjWQEBxA_DDi_FyT8',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwYmd2cHFqbnVvcndxd2psb2lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNjA2ODYsImV4cCI6MjA4NjYzNjY4Nn0.lTeXZte_rXZbS5VyeH1vM8k2mr-qGE33rJpfWXYZPmI',
     AIRTABLE_BASE_ID: (typeof window !== 'undefined' && window.__AIRTABLE_BASE_ID__) || '',
     AIRTABLE_API_KEY: (typeof window !== 'undefined' && window.__AIRTABLE_API_KEY__) || '',
 };
